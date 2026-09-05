8cf81b50c54abe91bfe81ada23a0821f2a6eb0cf
piyush <hellopiyush.lab@gmail.com>
2026-09-05 08:14:12 +0000

    new branch for ui prototype

--- src/index.js
+++ src/index.js
@@ -1,43 +1,42 @@
 // index.js
+import "./style.css";
+// import takeInput from "./take-input.js";
+// import { generateTaskInDOM, loadBackground, loadBaseElements, removeElements, generateWindow } from "./dom-manipulation.js";
+// import { saveData, getData } from "./data.js";
 
-import takeInput from "./take-input.js";
-import { generateTaskInDOM, loadBackground, loadBaseElements, removeElements, generateWindow } from "./dom-manipulation.js";
-import { saveData, getData } from "./data.js";
+// // loading a background
+// loadBackground(); 
 
-// loading a background
-loadBackground(); 
+// /// load the base elements, 
+// /// and then take the add task button from it
+// const baseElements = loadBaseElements();
+// const addTaskButton = baseElements.addTaskButton;
 
-/// load the base elements, 
-/// and then take the add task button from it
-const baseElements = loadBaseElements();
-const addTaskButton = baseElements.addTaskButton;
+// // now put event listner on the button
+// addTaskButton.addEventListener("click", (e) => {
 
-// now put event listner on the button
-addTaskButton.addEventListener("click", (e) => {
+//     // when the button is clicked, the window appears
+//     let generatedElements = generateWindow();
 
-    // when the button is clicked, the window appears
-    let generatedElements = generateWindow();
+//     // when i submit, the elements go to saveData
+//     generatedElements.submitButton.addEventListener("click", () => {
 
-    // when i submit, the elements go to saveData
-    generatedElements.submitButton.addEventListener("click", () => {
+//         // save the data, and get latest task
+//         let latestTask = saveData(generatedElements);
 
-        // save the data, and get latest task
-        let latestTask = saveData(generatedElements);
+//         // generate the task on dom
+//         console.log(latestTask);
+//         generateTaskInDOM(latestTask);
 
-        // generate the task on dom
-        console.log(latestTask);
-        generateTaskInDOM(latestTask);
+//         // remove the window after submission
+//         removeElements(generatedElements);
+//     });
 
-        // remove the window after submission
-        removeElements(generatedElements);
-    });
-
-    // remove the window if clicked outside
-    generatedElements.blurBackground.addEventListener("click", () => {
-        removeElements(generatedElements);
-    })
+//     // remove the window if clicked outside
+//     generatedElements.blurBackground.addEventListener("click", () => {
+//         removeElements(generatedElements);
+//     })
 
 
 
-});
-
+// });
